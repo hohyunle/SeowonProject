245e2b7aa74930ae8b20ea03f61fc6b3a21edb57
Nochang0 <hy040504@kakao.com>
2023-11-08 02:53:25 +0000

    make it better

--- test.c
+++ test.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-struct test {
-    int i;
-    char name[20];
-};
+int main(void)
+{
+    char a, b, c, d;
+    int num = 0;
 
-int main() {
-    struct test s1 = { 20, "연승현" };
-    printf("%s", s1.name);
+    printf("문자 입력(띄어쓰기로 구분) >> ");
+    scanf("%c %c %c %c", &a, &b, &c, &d);
+    num = (num << 8) | a; 
+    num = (num << 8) | b;
+    num = (num << 8) | c;
+    num = (num << 8) | d;
+
+
+    printf("입력한 문자 >> %c%c%c%c\n", (num>>24), (num>>16), (num>>8), num);
+
     return 0;
 }
