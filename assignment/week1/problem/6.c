3f9a6d1167c5be965d3701f647a128b347276904
Nochang0 <hy040504@kakao.com>
2023-11-08 01:51:00 +0000

    make it better

--- assignment/week1/problem/6.c
+++ assignment/week1/problem/6.c
@@ -8,35 +8,23 @@
 
 int main(void)
 {
-    char char1, char2, char3, char4;
-    int number;
+    char a, b, c, d;						// 입력받을 4개의 변수를 저장하는 변수
+    int num = 0;							// 하나로 저장할 정수 변수
 
-    printf("첫 번째 정수를 입력하시오 : ");
-    scanf("%c", &char1);
-    printf("두 번째 정수를 입력하시오 : ");
-    scanf(" %c", &char2);
-    printf("세 번째 정수를 입력하시오 : ");
-    scanf(" %c", &char3);
-    printf("네 번째 정수를 입력하시오 : ");
-    scanf(" %c", &char4);
+    printf("문자 입력(띄어쓰기로 구분) >> ");	
+    scanf("%c %c %c %c", &a, &b, &c, &d);	// 문자 4개 입력받기
 
-    if (char1 >= '0' && char1 <= '9') {
-        number = (char1 - '0');
-    }
-    if (char2 >= '0' && char2 <= '9') {
-        number = number * 10 + (char2 - '0');
-    }
-    if (char3 >= '0' && char3 <= '9') {
-        number = number * 10 + (char3 - '0');
-    }
-    if (char4 >= '0' && char4 <= '9') {
-        number = number * 10 + (char4 - '0');
-    }
+    // num 변수를 8비트(1바이트)씩 왼쪽으로 저장한 후, a 변수와 비트 OR 연산을 수행하여 num에 결합 (`b`, `c`, `d` 포함)
+    num = (num << 8) | a;
+    num = (num << 8) | b;
+    num = (num << 8) | c;
+    num = (num << 8) | d;
 
-    printf("%d\n", number);
+    printf("입력한 문자 >> %c%c%c%c\n", (num>>24), (num>>16), (num>>8), num);
 
     return 0;
 }
+
 
 
 
