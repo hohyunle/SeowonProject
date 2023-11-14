715f1fda3ffb4e83f78e399b570601237f1f5ab9
Nochang0 <hy040504@kakao.com>
2023-11-14 15:01:27 +0000

    fucking c

--- project/lib/json.h
+++ project/lib/json.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <json-c/json.h>
 
+
 // JSON 데이터를 합치는 함수
 char* addIncomeList(char* jsonData, char* actionList, char* listId, char* HistoryData) {
     struct json_object* root;           // 전체 리스트 내역
@@ -20,12 +21,6 @@ char* addIncomeList(char* jsonData, char* actionList, char* listId, char* Histor
 
     // "수입목록" 객체 가져오기
     json_object_object_get_ex(root, actionList, &typeList);
-    
-//     if (!json_object_object_get_ex(root, actionList, &typeList)) {
-//         // "수입목록" 객체가 없으면 새로 생성
-//         typeList = json_object_new_object();
-//         json_object_object_add(root, actionList, typeList);
-//     }
 
     // "수입목록" 객체에 새로운 데이터 추가
     json_object_object_add(typeList, listId, listInfo);
@@ -35,46 +30,43 @@ char* addIncomeList(char* jsonData, char* actionList, char* listId, char* Histor
     // char *result = strdup(mergedJsonData);
     
     result = strdup(json_object_to_json_string(root));
-    printf("\n%s\n", result);
     // 메모리 정리
-    json_object_put(root);
-
+    // json_object_put(root);
+    // free(root);
     return result;
 }
 
-
 // 가계부 리스트 내역 추가
-/*
-char* addIncomeList(char* jsonData, char* actionList, int uniqueId, char* HistoryData) {
-    struct json_object* root;           // 전체 리스트 내역
-    struct json_object* typeList;       // 수입 | 지출내역
-    struct json_object* listInfo;       // 내역 세부내용
+char* addDataList(const char* jsonData, const char *actionList, const char *tagtype, int uniqueId, const char* HistoryData) {
+    struct json_object* root;
+    struct json_object* typeList;
+    struct json_object* tag;
+    struct json_object* newHistory;
     
-    // char* result;                    // 수정 및 리턴할 Json
-    char listId[99999];                 // 고유번호 문자열 전환
+    char* result;
+    char *listId = NULL;
     sprintf(listId, "%d", uniqueId);
     
     // JSON 문자열을 파싱하여 JSON 객체 생성
     root = json_tokener_parse(jsonData);
 
-    // 수입 | 지출내역 객체 획득
+    // actionList 객체 획득
     json_object_object_get_ex(root, actionList, &typeList);
 
-    // 새로운 데이터를 JSON 형식으로 변환하여 listInfo 객체 생성
-    listInfo = json_tokener_parse(HistoryData);
+    // tagtype 객체 획득
+    json_object_object_get_ex(typeList, tagtype, &tag);
 
-    // typeList 객체에 새로운 데이터 추가
-    json_object_object_add(typeList, listId, listInfo);
+    // 새로운 데이터를 JSON 형식으로 변환하여 newHistory 객체 생성
+    newHistory = json_tokener_parse(HistoryData);
+
+    // tagtype 객체에 새로운 데이터 추가
+    json_object_object_add(tag, listId, newHistory);
 
     // JSON 객체를 문자열로 변환하여 결과 반환
-    char *result = strdup(json_object_to_json_string(root));
+    result = strdup(json_object_to_json_string(root));
 
     // JSON 객체 및 할당된 메모리 해제
     json_object_put(root);
-    json_object_put(listInfo);
-    free(listInfo);
-    free(typeList);
+    free(newHistory);
     return result;
 }
-
-*/
--- project/test2.c
+++ project/test2.c
@@ -1,9 +1,10 @@
-#include <json-c/json.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <json-c/json.h>
 
-const char* addDataToJson(const char* jsonString) {
+char* addDataToJson(char* jsonString) {
     // Parse the input JSON string
-    json_object *root = json_tokener_parse(jsonString);
+    json_object* root = json_tokener_parse(jsonString);
 
     // Check if parsing was successful
     if (is_error(root)) {
@@ -12,21 +13,31 @@ const char* addDataToJson(const char* jsonString) {
     }
 
     // Get the "수입목록" array
-    json_object *incomeList = json_object_object_get(root, "수입목록");
+    json_object* incomeList = NULL;
+    if (!json_object_object_get_ex(root, "수입목록", &incomeList)) {
+        // "수입목록" 객체가 없으면 새로 생성
+        incomeList = json_object_new_object();
+        json_object_object_add(root, "수입목록", incomeList);
+    }
 
     // Create a new entry
-    json_object *newEntry = json_object_new_object();
+    json_object* newEntry = json_object_new_object();
     json_object_object_add(newEntry, "날짜", json_object_new_string("2023-11-09"));
     json_object_object_add(newEntry, "금액", json_object_new_string("5000"));
     json_object_object_add(newEntry, "수입처", json_object_new_string("용돈"));
-    json_object_object_add(newEntry, "메모", json_object_new_string(""));
+    json_object_object_add(newEntry, "메모", json_object_new_string("1"));
     json_object_object_add(newEntry, "카테고리", json_object_new_string("tag2"));
 
+    // Generate a unique key for the new entry (e.g., "3")
+    int uniqueId = json_object_array_length(incomeList) + 1;
+    char key[16];
+    snprintf(key, sizeof(key), "%d", uniqueId);
+
     // Add the new entry to the "수입목록" array
-    json_object_object_add(incomeList, "3", newEntry);
+    json_object_object_add(incomeList, key, newEntry);
 
     // Get the modified JSON string
-    const char *modifiedJsonString = json_object_to_json_string_ext(root, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
+    const char* modifiedJsonString = json_object_to_json_string_ext(root, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
 
     // Free the JSON object
     json_object_put(root);
@@ -35,14 +46,16 @@ const char* addDataToJson(const char* jsonString) {
 }
 
 int main() {
-    const char *originalJson = "{\"수입목록\":{\"1\":{\"Date\":\"2023-11-09\",\"금액\":\"2000000\",\"수입처\":\"월급\",\"메모\":\"\",\"카테고리\":\"tag1\"},\"2\":{\"날짜\":\"2023-11-09\",\"금액\":\"5000\",\"수입처\":\"용돈\",\"메모\":\"\",\"카테고리\":\"tag2\"}}}";
+    char* originalJson = "{\"수입목록\":{\"1\":{\"Date\":\"2023-11-09\",\"금액\":\"2000000\",\"수입처\":\"월급\",\"메모\":\"\",\"카테고리\":\"tag1\"},\"2\":{\"날짜\":\"2023-11-09\",\"금액\":\"5000\",\"수입처\":\"용돈\",\"메모\":\"\",\"카테고리\":\"tag2\"}}}";
 
-    const char *modifiedJson = addDataToJson(originalJson);
+    char* modifiedJson = addDataToJson(originalJson);
 
-    printf("Modified JSON:\n%s\n", modifiedJson);
+    printf("Modified JSON: %s\n", modifiedJson);
 
     return 0;
 }
+
+
 
 
 
